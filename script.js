d38809016d7a308c5144ae9cf073d7ac0673dcf2
mansi2020 <sorathiyamansi11@gmail.com>
2023-10-09 00:21:50 +0530

    done the project

---
+++ script.js
@@ -0,0 +1,25 @@
+let keypress= document.getElementById("k1");
+let keyCodeofPress = document.getElementById("keyCodeofPress");
+let main = document.getElementById("main");
+let main2 = document.getElementsByClassName("main2");
+let k1 = document.getElementById("k1");
+
+//click event
+main.addEventListener('click',()=>{
+    main2[0].style.display = "block";
+    main.style.display = "none";
+});
+
+let beat = new Audio('6-september-2022-128k-60971.mp3');
+
+//keypress event
+document.addEventListener("keypress",(e)=>{
+    k1.innerText = e.key;
+    keyCodeofPress.innerText = e.keyCode;
+    beat.play();
+});
+
+
+
+
+
